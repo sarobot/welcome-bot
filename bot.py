129db14003cfa2855330fea35ce701e3e3578919
Saro <81003152+sarobot@users.noreply.github.com>
2021-04-14 19:25:46 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,41 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext,Filters,MessageHandler
+
+updater = Updater( "1651834967:AAH7MNyXOr628fYAWA4LHBYCHuYCQZETCuU",use_context = True )
+
+def start(updater,context):
+
+ updater.message.reply_text('''hi  iam saro welcome messanger bot 
+
+ Add me to your group 
+
+ 
+
+ Made with Love 
+
+ 
+
+ 
+
+  ''')
+
+ 
+
+def add_group(update: Update, context: CallbackContext):
+
+    for member in update.message.new_chat_members:
+
+        update.message.reply_text(f" Hello {member.full_name}  ")
+
+add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
+
+dp =updater.dispatcher.add_handler
+
+dp(CommandHandler('start',start))
+
+dp.add_handler(add_group_handle)
+
+updater.start_polling()
+
+updater.idle()
