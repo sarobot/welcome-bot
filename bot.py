dda91c58c5a88d3cacc3265370ff82f58e64776f
lokamanchendekar <82510059+lokamanchendekar@users.noreply.github.com>
2021-04-14 18:23:11 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext,Filters,MessageHandler
 
-updater = Updater( "1651834967:AAH7MNyXOr628fYAWA4LHBYCHuYCQZETCuU",use_context = True )
+updater = Updater( "1651834967:AAH7MNyXOr628fYAWA4LHBYCHuYCQZETCgK",use_context = True )
 
 def start(updater,context):
- updater.message.reply_text('''hi saro iam welcome messanger bot 
+ updater.message.reply_text('''hi  iam saro welcome messanger bot 
  Add me to your group 
  
- வணக்கம் நண்பர்களே நான் உங்களை என் குழுவில் வரவேற்பதற்கு மிகவும் மகிழ்ச்சி அடைகிறேன்😍😍
-
+ Made with Love ❤️ by @lntechnical
  
  
   ''')
@@ -16,12 +15,12 @@ def start(updater,context):
 
 def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
-        update.message.reply_text(f" Hello {member.full_name} வணக்கம் நண்பர்களே நான் உங்களை என் குழுவில் வரவேற்பதற்கு மிகவும் மகிழ்ச்சி அடைகிறேன்😍😍❤️")
+        update.message.reply_text(f" Hello {member.full_name}  ")
 
 add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
 
 dp =updater.dispatcher.add_handler
 dp(CommandHandler('start',start))
 dp.add_handler(add_group_handle)
-
-that's a complete code, what else you need?
+updater.start_polling()
+updater.idle()
